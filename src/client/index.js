e1df0851d437f88da2005d7f05c8d4659bb5adf2
Rizwan <rsaddique82@gmail.com>
2021-05-30 20:27:35 +0200

    first commit news

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import "./styles/header.scss";
+import "./styles/form.scss";
+import "./styles/footer.scss";
+import "./styles/resets.scss"; 
+import './styles/base.scss'
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
