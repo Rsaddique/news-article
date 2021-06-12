8548f0bfe03ccb2269001f190220ad29d85bff52
Rizwan <rsaddique82@gmail.com>
2021-06-12 21:34:46 +0200

    done project

--- src/server/index.js
+++ src/server/index.js
@@ -24,11 +24,9 @@ app.use(bodyParser.urlencoded({
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('dist/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,13 +14,39 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
-        ]
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                use: {
+                    loader: 'file-loader'
+                }
+              },
+              {
+                test: /\.(woff|woff2|ttf|eot|svg)$/,
+                use: {
+                    loader: 'url-loader'
+                }},
+                {
+                test: /\.html$./,
+                use: {
+                    loader: 'html-loader' }
+                }
+            ]
     },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
+            plugins: [
+                new HtmlWebPackPlugin({
+                    template: "./src/client/views/index.html",
+                    filename: "./index.html",
+                }),
+                new MiniCssExtractPlugin({filename: '[name].css'}),
+                new WorkboxPlugin.GenerateSW()
+            ],
+            output:{
+                libraryTarget: 'var',
+                library: 'Client'
+            }
 }
